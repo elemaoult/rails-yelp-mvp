f5a05e4464e8e9e037b78a5c969cb9881357fc3d
Emilie LeMaoult <emilie.lemaoult@gmail.com>
2021-09-23 19:45:46 +0200

    Solving Yelp

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,7 @@ class Review < ApplicationRecord
 
   @allowed_ratings = [0, 1, 2, 3, 4, 5]
 
-  validates :rating, presence: true, inclusion: { in: @allowed_ratings }
+  validates :rating, presence: true, inclusion: { in: @allowed_ratings }, numericality: { only_integer: true }
   validates :content, presence: true
 
 end
